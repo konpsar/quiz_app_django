c476a00755597407b85fa7af2831db11fa1f6153
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-21 21:15:40 +0300

    Create two question models
    Model for multiple choice Q with 4 options and Model for Open Qs

--- quiz_project/Quiz/models.py
+++ quiz_project/Quiz/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class OpenQuesModel(models.Model):
+    question = models.CharField(max_length=250)
+    answer = models.CharField(max_length=250, null=True)    
+    correct_ans = models.CharField(max_length=250)    
+
+class MultQuesModel(models.Model):
+    question = models.CharField(max_length=250)
+    opt1 = models.CharField(max_length=200,null=True)
+    opt2 = models.CharField(max_length=200,null=True)
+    opt3 = models.CharField(max_length=200,null=True)
+    opt4 = models.CharField(max_length=200,null=True)
+    correct_ans = models.CharField(max_length=250)
