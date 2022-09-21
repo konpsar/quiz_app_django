4636fdd7b26be219788e93d4926c305e3bc45614
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-21 21:22:20 +0300

    Register Ques models to admin page + create superuser

--- quiz_project/Quiz/admin.py
+++ quiz_project/Quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from quiz_project.Quiz.models import MultQuesModel, OpenQuesModel
+
 # Register your models here.
+admin.site.register(OpenQuesModel)
+admin.site.register(MultQuesModel)
