0e79802729936383bc1990e7b19a8ee7e628ceb9
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-22 15:48:44 +0300

    Add 'Quiz' to apps in settings.py

--- quiz_project/Quiz/admin.py
+++ quiz_project/Quiz/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from quiz_project.Quiz.models import MultQuesModel, OpenQuesModel
+from .models import MultQuesModel, OpenQuesModel
 
 # Register your models here.
 admin.site.register(OpenQuesModel)
