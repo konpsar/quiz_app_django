8727d395b00c0f96e28d553cc8bc7d51c719e211
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-22 13:48:34 +0300

    Hide django secret_key (configuration ok for dev_env, not production)
    secret_key is now saved in file .env (not pushed in repo) and  imported in settings.py using decouple library

--- quiz_project/quiz_project/settings.py
+++ quiz_project/quiz_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^fkz4$3(j#$@0ovv)5uvl+icho^0nc+gig(j)o_hdu!gci=44m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
