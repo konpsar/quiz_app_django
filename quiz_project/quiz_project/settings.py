308c545b1a9de875ffc7d96f694b921dfe1d9d94
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-09-23 00:27:33 +0300

    Create 'home' app and a simple welcome page
    set as landing page the simple welcome page created in home app

--- quiz_project/quiz_project/settings.py
+++ quiz_project/quiz_project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #
+    #My_Apps
     'Quiz',
+    'home',
 ]
 
 MIDDLEWARE = [
