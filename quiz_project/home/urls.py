a3a374890a7c42a4c4f41a13c5e5201203bc8bf0
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-10-03 21:11:06 +0300

    Use Bootstrap in welcome page + create signup page (+signupView)

--- quiz_project/home/urls.py
+++ quiz_project/home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name="home"),
+    path('signup', views.SignupView.as_view(), name="signup"),
+    
 ]
--- quiz_project/home/views.py
+++ quiz_project/home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from datetime import datetime
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name  = 'home/welcome.html'
     extra_context = {'today':datetime.today()}
+    
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name  = 'home/signup.html'
+    extra_context = {}
+    
