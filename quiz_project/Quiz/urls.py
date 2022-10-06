8f6a711e72f671c1687cfa39126bde7eccf989c7
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-10-07 02:34:50 +0300

    Create a very simple list view for open ended and multiple choice Qs

--- quiz_project/Quiz/urls.py
+++ quiz_project/Quiz/urls.py
@@ -3,8 +3,11 @@ from . import views
 from Quiz.views import OpenQuesCreateView, MultQuesCreateView
 
 urlpatterns = [
-    path('addQues/multi/', MultQuesCreateView.as_view(), name='MultiQues-add'),
-    path('addQues/open/', OpenQuesCreateView.as_view(), name='OpenQues-add'),
+    path('Ques/add/multi/', MultQuesCreateView.as_view(), name='MultiQues-add'),
+    path('Ques/add/open/', OpenQuesCreateView.as_view(), name='OpenQues-add'),
+    path('Ques/list/multi', views.QuesListView.as_view(), name="multiQues.list"),
+    path('Ques/list/open', views.OpenQuesListView.as_view(), name="openQues.list"),
+
 ]
 
 
--- quiz_project/Quiz/views.py
+++ quiz_project/Quiz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from Quiz.forms import *
 from Quiz.models import *
 
@@ -17,3 +17,28 @@ class MultQuesCreateView(CreateView):
     fields = '__all__'
     template_name  = 'Quiz/addQues.html'
     success_url = '/admin/Quiz/openquesmodel/'
+
+
+class QuesListView(ListView):
+    model = MultQuesModel
+    context_object_name = "multiQs"
+    template_name = "Quiz/multi_ques_list.html"
+    
+    #Search on ccbv.co.uk/projects/Django/3.1/ to get help on class views and find what 
+    #we should alter in order to get only specific user's notes
+
+    def get_queryset(self):
+        return MultQuesModel.objects.all()
+    
+class OpenQuesListView(ListView):
+    model = OpenQuesModel
+    context_object_name = "openQs"
+    template_name = "Quiz/open_ques_list.html"
+    
+    #Search on ccbv.co.uk/projects/Django/3.1/ to get help on class views and find what 
+    #we should alter in order to get only specific user's notes
+
+    def get_queryset(self):
+        return OpenQuesModel.objects.all()
+    
+    
