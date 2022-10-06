f28d7eef7122a7dad5fcf3bfb3cf4941cc148bb7
Konstantinos Psaroulakis <konstantinos.psaroulakis@gmail.com>
2022-10-06 23:59:31 +0300

    add success url and class_form to forms views

--- quiz_project/Quiz/views.py
+++ quiz_project/Quiz/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from Quiz.forms import *
 from Quiz.models import *
 
 # Create your views here.
 class OpenQuesCreateView(CreateView):
     model = OpenQuesModel
+    class_form = addOpenQuestionform
     fields = '__all__'
     template_name  = 'Quiz/addQues.html'
-
+    success_url = '/admin/Quiz/openquesmodel/'
 
 class MultQuesCreateView(CreateView):
     model = MultQuesModel
+    class_form = addMultiQuestionform
     fields = '__all__'
     template_name  = 'Quiz/addQues.html'
+    success_url = '/admin/Quiz/openquesmodel/'
